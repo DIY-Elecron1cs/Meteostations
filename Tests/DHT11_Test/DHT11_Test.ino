b39512ad264364ed5b3b27e73e539c76544bc02e
DIY электроника <private.property240702@gmail.com>
2024-10-17 16:24:19 +0300

    Create DHT11_Test.ino

---
+++ Tests/DHT11_Test/DHT11_Test.ino
@@ -0,0 +1,28 @@
+#include <DHT11.h>
+DHT11 dht11(2);
+
+void setup(){
+  Serial.begin(9600);
+}
+void loop(){
+  int temperature = dht11.readTemperature();
+  int humidity = dht11.readHumidity();
+
+  if (temperature != DHT11::ERROR_CHECKSUM && temperature != DHT11::ERROR_TIMEOUT){
+    Serial.print("температура: ");
+    Serial.print(temperature);
+    Serial.println(" °C");
+  }
+  else{
+    Serial.println(DHT11::getErrorString(temperature));
+  }
+  if (humidity != DHT11::ERROR_CHECKSUM && humidity != DHT11::ERROR_TIMEOUT){
+    Serial.print("влажность: ");
+    Serial.print(humidity);
+    Serial.println(" %");
+  }
+  else{
+    Serial.print(DHT11::getErrorString(humidity));
+  }
+  delay(1000);
+}
